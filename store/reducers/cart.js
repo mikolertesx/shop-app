366c42fd19c2528cf12f2bca055a351af2dfd000
Mikolertesx <mgangsal@outlook.com>
2020-11-26 10:29:29 -0600

    Clearing the Cart

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,4 +1,5 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from "../actions/cart";
+import { ADD_ORDER } from "../actions/orders";
 import CartItem from "../../models/cart-item";
 
 const initialState = {
@@ -59,6 +60,9 @@ export default (state = initialState, action) => {
             ? 0
             : state.totalAmount - selectedCartItem.productPrice,
       };
+
+    case ADD_ORDER:
+      return initialState;
   }
 
   return state;
