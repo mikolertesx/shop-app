3fd5a88c264fc594b326c6fda0257ca5e7982b09
Mikolertesx <mgangsal@outlook.com>
2020-11-27 11:02:21 -0600

    Addiung Basic Editing & Navigation Logic

---
+++ screens/user/EditProductScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const EditProductScreen = props => {
+  return (
+    <View>
+      <Text>The edit Product Screen!</Text>
+    </View>
+  );
+};
+
+export default EditProductScreen;
