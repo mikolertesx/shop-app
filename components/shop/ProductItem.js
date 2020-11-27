bd2bca3ed760a568c0d610912c1917d988b125d2
Mikolertesx <mgangsal@outlook.com>
2020-11-27 12:48:11 -0600

    Refactoring CARD UI

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -9,6 +9,8 @@ import {
   Platform,
 } from "react-native";
 
+import Card from "../UI/Card";
+
 const ProductItem = ({
   image,
   title,
@@ -24,7 +26,7 @@ const ProductItem = ({
   }
 
   return (
-    <View style={styles.product}>
+    <Card style={styles.product}>
       <View style={styles.touchable}>
         <TouchableCmp onPress={onSelect} useForeground>
           <View>
@@ -39,19 +41,12 @@ const ProductItem = ({
           </View>
         </TouchableCmp>
       </View>
-    </View>
+    </Card>
   );
 };
 
 const styles = StyleSheet.create({
   product: {
-    shadowColor: "black",
-    shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 8,
-    elevation: 5,
-    borderRadius: 10,
-    backgroundColor: "white",
     height: 300,
     margin: 20,
     overflow: "hidden",
--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -45,6 +45,7 @@ const EditProductScreen = (props) => {
         productsActions.createProduct(title, description, imageUrl, +price)
       );
     }
+    props.navigation.goBack();
   }, [dispatch, title, imageUrl, price, description, prodId]);
 
   useEffect(() => {
